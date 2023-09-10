f8ca91cdb3e525a23736899da23a496d3759c4c2
PatrickFS18 <Patrick.souza@aluno.riogrande.ifrs.edu.br>
2023-09-10 16:37:18 -0300

    organizando o código

--- src/controllers/personagensController.ts
+++ src/controllers/personagensController.ts
@@ -1,170 +1,215 @@
 import { Request, Response } from "express";
-
 import { PrismaClient } from "../../prisma/generated/client";
 
 const prisma = new PrismaClient();
 
+//minha class controller, firstController
+
 class firstController {
+  
   //edit page init
-
   public async EditPageInit(req: Request, res: Response) {
-    const characterId = req.body.id;
-    const character = await prisma.character.findUnique({
-      where: {
-        id: Number(characterId),
-      },
-    });
-    res.render("editCharacters", { character });
-    await prisma.$disconnect();
-    }
 
+    try {
+
+      const characterId = req.body.id;
+
+      const character = await prisma.character.findUnique({
+        where: {
+          id: Number(characterId),
+        },
+      });
+
+      res.render("editCharacters", { character });
+
+    } catch (error) {
+
+      const err = {
+        success: false,
+        error: "Ocorreu um erro!. Aguarde alguns instantes..",
+      };
+
+      console.error("Ocorreu um erro!", error);
+      res.render("errorPage", { err });
+
+    } finally {
+
+      await prisma.$disconnect();
+
+    }
+  }
   //deletar personagem
 
   public async deletarPersonagens(req: Request, res: Response) {
-    try{
-    const characters = prisma.character.findMany();
 
-    const { id } = req.params;
-    const Acharacter = await prisma.character.delete({
+    try {
+      const characters = prisma.character.findMany();
+
+      const { id } = req.params;
+      const Acharacter = await prisma.character.delete({
         where: { id: Number(id) },
-    });
-    
-    res.redirect("/");
-  }catch(error) {
-    const err={
-      success: false,
-      error: 'Erro ao excluir o personagem. Aguarde alguns instantes..',
-    };
-      console.error('Erro ao excluir o personagem:', error);
-      res.render("errorPage",{ err });
-  }
-  finally {
+      });
+
+      res.redirect("/");
+
+    } catch (error) {
+
+      const err = {
+        success: false,
+        error: "Erro ao excluir o personagem. Aguarde alguns instantes..",
+      };
+
+      console.error("Erro ao excluir o personagem:", error);
+      res.render("errorPage", { err });
+
+    } finally {
+
       await prisma.$disconnect();
+
     }
+  }
+
+  // inserir persongem (init page)
+  public InitPageAdd(req: Request, res: Response) {
+
+    res.render("addCharacters");
+
   }
 
   //inserir Personagem
   public async inserirPersonagens(req: Request, res: Response) {
-    const characters = prisma.character.findMany();
-
-    const id = req.body.id;
-const name = req.body.name;
-const species = req.body.species;
-const status = req.body.status;
-const type = req.body.type;
-const gender = req.body.gender;
-const originName = req.body.originName;
-const originLink = req.body.originLink;
-const locationName = req.body.locationName;
-const locationLink = req.body.locationLink;
-const image = req.body.image;
-const created = req.body.created;
-const url = req.body.url;
-
-    const newCharacter = prisma.character.create({
-      data: {
-        name,
-        species,
-        status,
-        type,
-        gender,
-        originName,
-        originLink,
-        locationName,
-        locationLink,
-        image,
-        created,
-        url,
-      },
-    });
-    
-    res.render("allCharacters", { characters });
-    
-  }
-
-  public async listarPersonagens(req: Request, res: Response) {
     try {
-      const characters = await prisma.character.findMany();
+      const characters = prisma.character.findMany();
+
+      const id = req.body.id;
+      const name = req.body.name;
+      const species = req.body.species;
+      const status = req.body.status;
+      const type = req.body.type;
+      const gender = req.body.gender;
+      const originName = req.body.originName;
+      const originLink = req.body.originLink;
+      const locationName = req.body.locationName;
+      const locationLink = req.body.locationLink;
+      const image = req.body.image;
+      const created = req.body.created;
+      const url = req.body.url;
+
+      const newCharacter = await prisma.character.create({
+        data: {
+          name,
+          species,
+          status,
+          type,
+          gender,
+          originName,
+          originLink,
+          locationName,
+          locationLink,
+          image,
+          created,
+          url,
+        },
+      });
 
       res.render("allCharacters", { characters });
-    } catch(error) {
-      const err={
-        success: false,
-        error: 'Erro ao listar os personagens. Aguarde alguns instantes..',
-      };
-        console.error('Erro ao listar o personagem:', error);
-        res.render("errorPage",{ err });
+
+    } catch (error) {
+
+      const err={ error };
+      res.render("errorPage",{ err });
+
     } finally {
+
       await prisma.$disconnect();
+
     }
   }
 
-public async editarPersonagens(req: Request, res: Response) {
-  const characters = prisma.character.findMany();
+  // listar personagem 
 
-  const id = req.body.id;
-  const name = req.body.name;
-  const species = req.body.species;
-  const status = req.body.status;
-  const type = req.body.type;
-  const gender = req.body.gender;
-  const originName = req.body.originName;
-  const originLink = req.body.originLink;
-  const locationName = req.body.locationName;
-  const locationLink = req.body.locationLink;
-  const image = req.body.image;
-  const created = req.body.created;
-  const url = req.body.url;
+  public async listarPersonagens(req: Request, res: Response) {
 
-  console.log('id:', id);
-  console.log('name:', name);
-  console.log('species:', species);
-  console.log('status:', status);
-  console.log('type:', type);
-  console.log('gender:', gender);
-  console.log('originName:', originName);
-  console.log('originLink:', originLink);
-  console.log('locationName:', locationName);
-  console.log('locationLink:', locationLink);
-  console.log('image:', image);
-  console.log('created:', created);
-  console.log('url:', url);
-  
+    try {
 
-  try {
+      const characters = await prisma.character.findMany();
+
+      res.render("allCharacters", { characters });
+
+    } catch (error) {
+
+      const err = {
+        success: false,
+        error: "Erro ao listar os personagens. Aguarde alguns instantes..",
+      };
+
+      console.error("Erro ao listar o personagem:", error);
+      res.render("errorPage", { err });
+
+    } finally {
+
+      await prisma.$disconnect();
+
+    }
+  }
+
+
+  //editar personagem
+
+  public async editarPersonagens(req: Request, res: Response) {
+
+    try {
+    const characters = prisma.character.findMany();
+
+    const id = req.body.id;
+    const name = req.body.name;
+    const species = req.body.species;
+    const status = req.body.status;
+    const type = req.body.type;
+    const gender = req.body.gender;
+    const originName = req.body.originName;
+    const originLink = req.body.originLink;
+    const locationName = req.body.locationName;
+    const locationLink = req.body.locationLink;
+    const image = req.body.image;
+    const created = req.body.created;
+    const url = req.body.url;
+
     const updatedCharacter = await prisma.character.update({
-      where: { id: Number(id) },
-      data: {
-        name,
-        species,
-        status,
-        type,
-        gender,
-        originName,
-        originLink,
-        locationName,
-        locationLink,
-        image,
-        created,
-        url,
-      },
+        where: { id: Number(id) },
+        data: {
+          name,
+          species,
+          status,
+          type,
+          gender,
+          originName,
+          originLink,
+          locationName,
+          locationLink,
+          image,
+          created,
+          url,
+        },
     });
 
     res.redirect("/");
-  } catch(error) {
-    const err={
-      success: false,
-      error: 'Erro ao editar o personagem. Aguarde alguns instantes..',
-    };
-      console.error('Erro ao editar o personagem:', error);
-      res.render("errorPage",{ err });
+    
+  } catch (error) {
+
+      const err = {
+        success: false,
+        error: "Erro ao editar o personagem. Aguarde alguns instantes..",
+      };
+      console.error("Erro ao editar o personagem:", error);
+      res.render("errorPage", { err });
+    
+    } finally {
+
+      await prisma.$disconnect();
+
+    }
   }
-  finally {
-    await prisma.$disconnect();
-  }
-};
 }
 
 export const FirstController = new firstController();
-
-
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,20 +1,24 @@
 import { Router } from 'express';
 import { PrismaClient } from '../../prisma/generated/client'; 
 import { FirstController } from '../controllers/personagensController';
+
 const router = Router();
 const prisma = new PrismaClient();
 
+// -----------------------GET-------------------------------//
 
 router.get('/', FirstController.listarPersonagens);
+router.get('/addCharacter', FirstController.InitPageAdd);
 
-//router.post('/editPage', initPageEditar);
+//--Delete--//
+router.get('/delete/:id', FirstController.deletarPersonagens);
+
+// ----------------------POST-------------------------------//
+
 router.post('/editPage', FirstController.EditPageInit);
-
-router.post('/characters', FirstController.inserirPersonagens);
-
+router.post('/addCharacter', FirstController.inserirPersonagens);
 router.post('/editCharacter', FirstController.editarPersonagens);
 
-router.get('/delete/:id', FirstController.deletarPersonagens);
 
 
 export { router as personagensRoutes };
